0b3a7b6cfbab5f2ac281200fb3a593f2ec7134ec
Jorge Castillo <jjoorrssxp@outlook.com>
2018-12-16 19:06:54 -0600

    Fix name error

--- Numbers/Fibonacci Sequence/index.c
+++ Numbers/Fibonacci Sequence/index.c
@@ -1,5 +1,4 @@
-/* Enter a number and have the program generate PI up to that many decimal places. 
- Keep a limit to how far the program will go. */
+// Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
 
 #include <stdio.h>
 
